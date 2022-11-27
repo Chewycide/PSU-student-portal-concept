0e335fa08222a66c66f1ac2d5f01aa405264a0b8
Chewycide <gitchewycide@gmail.com>
2022-11-27 22:00:13 +0800

    Add HTML elements to login template

--- forms.py
+++ forms.py
@@ -6,8 +6,8 @@ from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     """Form used on the login page"""
     
-    psu_id = StringField('PSU ID', validators=[DataRequired(message='Insert your PSU ID'), Length(min=10, message='Invalid ID Length')])
-    password = PasswordField('PASSWORD', validators=[DataRequired()])
+    psu_id = StringField('PSU ID:', validators=[DataRequired(message='Insert your PSU ID'), Length(min=10, message='Invalid ID Length')])
+    password = PasswordField('PASSWORD:', validators=[DataRequired()])
     login = SubmitField('LOGIN')
 
     
