23fac0801fb93e05c436f72304efa45f0eee8bd7
Chewycide <gitchewycide@gmail.com>
2022-12-22 18:40:09 +0800

    Refactor Code

--- website/routes.py
+++ website/routes.py
@@ -17,25 +17,26 @@ def logout():
     logout_user()
     return redirect(url_for('login_page'))
 
-    
+
 # ---------- ROUTES ---------- #
 
-# Login route
 @app.route('/', methods=['POST', 'GET'])
 def login_page():
+    """Login route"""
     login_form = LoginForm()
     if login_form.validate_on_submit():
         return redirect(url_for('login_page'))
     return render_template('login.html', login_form = login_form)
 
 
-# Main Menu route
 @app.route('/mainmenu')
 def main_menu():
+    """Mainmenu route"""
     return render_template('main_menu.html')
 
 
-# Enrollment/Registration route
 @app.route('/enrollment')
 def enrollment():
+    """Enrollment route"""
     return render_template('enrollment.html')
+
