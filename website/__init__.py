c1abb46820a608212a6098bdf4b5a54754b699cc
Chewycide <gitchewycide@gmail.com>
2022-12-22 18:28:17 +0800

    Initialize Login Manager
    Login Manager is needed for loading the user from an ID taken from the
    database and for logging the user out of the website.

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 
 
 
@@ -6,6 +7,11 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.config['SECRET_KEY']
+
+
+
+# ---------- LOGIN MANAGER ---------- #
+login_manager = LoginManager(app)
 
 
 # ---------- ROUTES IMPORT ---------- #
