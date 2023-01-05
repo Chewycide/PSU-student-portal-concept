c7af46c8dc927cfb2987ac55a5c89f168642d7ef
Chewycide <gitchewycide@gmail.com>
2023-01-05 19:13:34 +0800

    Check Valid Format for PSU ID Field.
    PSU ID field is now able to check whether user input format is valid.

--- website/validators.py
+++ website/validators.py
@@ -1,5 +1,6 @@
 from website.models import Student
 from wtforms.validators import ValidationError
+from keyword import iskeyword
 from werkzeug.security import check_password_hash
 
 
@@ -29,3 +30,24 @@ def validate_user(form, field):
     user = Student.query.filter_by(student_id_number=form.psu_id.data).first()
     if not user:
         raise ValidationError(message="User does not exist. Please contact an Admin to register you to the database.")
+
+
+def validate_id(form, field):
+    """
+        Check if input is in a valid PSU ID format.
+    """
+    psu_id_format = "XX-YY-XXXX"
+    format_to_compare = ""
+    psu_id = form.psu_id.data
+
+    # Iterate through all the characters
+    for character in psu_id:
+        if character.isalpha():
+            format_to_compare += "Y"
+        elif character.isdigit():
+            format_to_compare += "X"
+        elif not character.isalpha() or not character.isdigit():
+            format_to_compare += "-"
+
+    if not psu_id_format == format_to_compare:
+        raise ValidationError("Not a valid PSU ID.")
