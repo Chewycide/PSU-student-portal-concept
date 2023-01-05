69f207d0ef0daded01f53e9fb7c195d83b25c79a
Chewycide <gitchewycide@gmail.com>
2023-01-05 19:16:03 +0800

    Refactor Code

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
-from website.validators import validate_password, validate_user
+from website.validators import validate_password, validate_user, validate_id
 
 
 # ---------- FORMS ---------- #
@@ -9,8 +9,9 @@ class LoginForm(FlaskForm):
     """Form used on the login page"""
     psu_id = StringField('PSU ID:',
         validators=[
-            DataRequired(message='Insert your PSU ID'),
-            Length(min=10, message='Invalid ID Length'),
+            DataRequired(message='Insert your PSU ID.'),
+            Length(min=10, message=''),
+            validate_id,
             validate_user
         ]
     )
@@ -23,7 +24,13 @@ class RegisterStudentForm(FlaskForm):
         This form is used to register a student to the database.
         only login information is required to enter.
     """
-    student_id_number = StringField('Student Id Number', validators=[DataRequired(), Length(min=10, message="Invalid Length")])
+    student_id_number = StringField(
+        'Student Id Number',
+        validators=[
+           DataRequired(),
+           Length(min=10, message="Invalid Length")
+        ]
+    )
     student_fullname = StringField('Full Name', validators=[DataRequired()])
     student_password = PasswordField('Student Password', validators=[DataRequired(), Length(min=7)])
     confirm_student_password = PasswordField('Confirm Student Password', validators=[DataRequired(), EqualTo('student_password', message='Password does not match.')])
